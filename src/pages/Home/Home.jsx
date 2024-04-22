df29f48c86f57dd1a902677a0d437722b9632a1f
estefania.martinez@est.iudigital.edu.co <estefania.martinez@est.iudigital.edu.co>
2024-04-22 09:33:20 -0500

    cambios en el home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div><h1>Inicio feed</h1></div>
-  )
-}
+    <div>
+      <h1>inicio findy</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
