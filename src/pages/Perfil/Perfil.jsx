b246f1b714c6fe65ecb7b43de83e42e849661992
Sara patiño <sarapatinorojas144@gmail.com>
2024-04-24 16:39:59 -0500

    imagenes listas

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import { createGlobalStyle } from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import { endpoints } from '../../services/data';
-
-
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { endpoints } from "../../services/data";
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap');
@@ -14,39 +12,36 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-
 // Estilos para la barra de estado del dispositivo
 const DeviceStatusBar = styled.div`
-  background-color: #f8f9fa; 
-  padding: 5px 10px; 
-  display: flex; 
-  justify-content: space-between; 
-  align-items: center; 
+  background-color: #f8f9fa;
+  padding: 5px 10px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
-
 
 // Icono  para la red, wifi y batería
 const Icon = styled.img`
-margin-right: 10px; 
+  margin-right: 10px;
 `;
 
 // Estilos para la hora en la barra de estado
 const Time = styled.span`
-  font-size: 1em; 
-  color: #333; 
-
+  font-size: 1em;
+  color: #333;
 `;
 
 const ProfileImageOverlay = styled.img`
-  border-radius: 50%; 
-  width: 75px; 
-  height: 75px; 
+  border-radius: 50%;
+  width: 75px;
+  height: 75px;
   object-fit: cover;
-  border: 3px solid white; 
-  position: absolute; 
-  bottom: 220px;  
-  left: 50%; 
-  transform: translateX(-50%); 
+  border: 3px solid white;
+  position: absolute;
+  bottom: 220px;
+  left: 50%;
+  transform: translateX(-50%);
   top: 250px;
 `;
 
@@ -56,37 +51,39 @@ const ProfileContainer = styled.div`
   align-items: center;
   padding: 20px;
   background-color: #fff;
-  background:  radial-gradient(50% 50% at 50% 50%, rgba(255, 118, 116, 0.6) 0%, rgba(255, 118, 116, 0) 100%),
-  rgba(248, 248, 248, 1);
-  width: 428px; 
-  max-width: 600px; 
-  margin: auto; 
+  background: radial-gradient(
+      50% 50% at 50% 50%,
+      rgba(255, 118, 116, 0.6) 0%,
+      rgba(255, 118, 116, 0) 100%
+    ),
+    rgba(248, 248, 248, 1);
+  width: 428px;
+  max-width: 600px;
+  margin: auto;
 `;
 
 const ProfileImage = styled.img`
-  border-radius: 20px; 
-  width: 100%; 
-  height: auto; 
-  object-fit: cover; 
+  border-radius: 20px;
+  width: 100%;
+  height: auto;
+  object-fit: cover;
   border: none;
-  box-shadow: 0 4px 8px rgba(0,0,0,0.1); 
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 `;
 
-
-
 const UserName = styled.h2`
-  font-size: 1.5em; 
+  font-size: 1.5em;
   margin-top: 40px;
   color: #333;
-  font-family: 'Balsamiq Sans', cursive;
+  font-family: "Balsamiq Sans", cursive;
 `;
 
 const UserInfo = styled.div`
   display: flex;
   justify-content: space-around;
-  width: 100%; 
+  width: 100%;
   margin-top: -110px;
-  font-family: 'Balsamiq Sans', cursive;
+  font-family: "Balsamiq Sans", cursive;
 `;
 
 const ButtonGroup = styled.div`
@@ -97,42 +94,42 @@ const ButtonGroup = styled.div`
 `;
 
 const FollowButton = styled.button`
-background: linear-gradient(92.69deg, #EB5E5C -0.64%, #FF7674 101.09%);
-color: white;
-border: none;
-padding: 5px 80px; // Aumenta el padding horizontal para que sean más alargados
-font-size: 20px; // El tamaño de fuente solicitado
-border-radius: 10px; // Ajusta según el diseño
-cursor: pointer;
-margin-top: 80px;
-font-family: 'Balsamiq Sans', cursive;
+  background: linear-gradient(92.69deg, #eb5e5c -0.64%, #ff7674 101.09%);
+  color: white;
+  border: none;
+  padding: 5px 80px; // Aumenta el padding horizontal para que sean más alargados
+  font-size: 20px; // El tamaño de fuente solicitado
+  border-radius: 10px; // Ajusta según el diseño
+  cursor: pointer;
+  margin-top: 80px;
+  font-family: "Balsamiq Sans", cursive;
 `;
 
 const MessageButton = styled(FollowButton)`
- background: linear-gradient(92.69deg, #EB5E5C -0.64%, #FF7674 101.09%);
+  background: linear-gradient(92.69deg, #eb5e5c -0.64%, #ff7674 101.09%);
 `;
 
 // Estilos para los tabs de navegación de fotos, videos,
 const ImageSectionContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-background: rgba(255, 255, 255, 1);
-width: 428px;
-max-width: 600px; 
-border-radius: 20px;
-padding: 20px;
-box-shadow: 0 4px 8px rgba(0,0,0,0.1); // Sombra para dar profundidad
-margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: rgba(255, 255, 255, 1);
+  width: 428px;
+  max-width: 600px;
+  border-radius: 20px;
+  padding: 20px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); // Sombra para dar profundidad
+  margin: auto;
 `;
 
 const Tabs = styled.div`
   display: flex;
   justify-content: space-around;
   margin-top: 20px;
-  max-width: 428px; 
-  margin: auto; 
-  font-family: 'Balsamiq Sans', cursive;
+  max-width: 428px;
+  margin: auto;
+  font-family: "Balsamiq Sans", cursive;
 `;
 
 const Tab = styled.button`
@@ -149,7 +146,7 @@ const Tab = styled.button`
   &.active {
     border-bottom: 2px solid pink;
   }
-  font-family: 'Balsamiq Sans', cursive;
+  font-family: "Balsamiq Sans", cursive;
 `;
 
 // Estilos para la grilla de imágenes
@@ -158,8 +155,8 @@ const ImageGrid = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(190px, 1fr));
   gap: 15px;
   margin-top: 20px;
-  max-width: 428px; 
-  margin: auto; 
+  max-width: 428px;
+  margin: auto;
 `;
 
 const ImageWrapper = styled.div`
@@ -169,9 +166,9 @@ const ImageWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-  width: 190px; 
-  height: 190px; 
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  width: 190px;
+  height: 190px;
 `;
 
 const Image = styled.img`
@@ -187,26 +184,43 @@ const ActiveTab = styled(Tab)`
 `;
 
 const UserProfile = () => {
-  const [activeTab, setActiveTab] = useState('Photos');
-  const [posts, setPosts] = useState([]);
+  const [activeTab, setActiveTab] = useState("Photos");
+  const [content, setContent] = useState({
+    photos: [],
+    videos: [],
+    albums: [],
+    tags: [],
+  });
   const [isFollowing, setIsFollowing] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const getAllPosts = async () => {
+    const getAllContent = async () => {
       try {
         const response = await fetch(endpoints.posts);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
         const data = await response.json();
-        setPosts(data);
+
+        // Depuración: Muestra los datos en la consola
+        console.log(data);
+
+        const photos = data.filter((item) => item.type === "photo");
+        const videos = data.filter((item) => item.type === "video");
+        const albums = data.filter((item) => item.type === "album");
+        const tags = data.filter((item) => item.type === "tag");
+
+        setContent({ photos, videos, albums, tags });
+
+        // Depuración: Muestra el contenido en la consola
+        console.log({ photos, videos, albums, tags });
       } catch (error) {
-        console.error("Error fetching posts: ", error);
+        console.error("Error fetching content: ", error);
       }
     };
 
-    getAllPosts();
+    getAllContent();
   }, []);
 
   const handleTabClick = (tabName) => {
@@ -233,10 +247,13 @@ const UserProfile = () => {
       </DeviceStatusBar>
       <ProfileContainer>
         <ProfileImage src="/public/blackpink-img-perfil.png" alt="Perfil" />
-        <ProfileImageOverlay src="/public/img-perfil-small.png" alt="Perfil pequeño" />
+        <ProfileImageOverlay
+          src="/public/img-perfil-small.png"
+          alt="Perfil pequeño"
+        />
         <UserName>Jennie Kim</UserName>
         <span>J. Hello Guys</span>
-          <span>Follow me and like my post</span>
+        <span>Follow me and like my post</span>
         <UserInfo>
           <span>10.7M Followers</span>
           <span>108.3M Likes</span>
@@ -244,50 +261,84 @@ const UserProfile = () => {
           <span>Follow me and like my post</span> */}
         </UserInfo>
         <div>
-        <ButtonGroup>
-        <FollowButton onClick={handleFollowClick}>
-          {isFollowing ? 'Following' : 'Follow'}
-        </FollowButton>
-          <MessageButton>Message</MessageButton>
+          <ButtonGroup>
+            <FollowButton onClick={handleFollowClick}>
+              {isFollowing ? "Following" : "Follow"}
+            </FollowButton>
+            <MessageButton>Message</MessageButton>
           </ButtonGroup>
         </div>
       </ProfileContainer>
       <ImageSectionContainer>
-      <Tabs>
-          <Tab onClick={() => handleTabClick('Photos')} className={activeTab === 'Photos' ? 'active' : ''}>
+        <Tabs>
+          <Tab
+            onClick={() => handleTabClick("Photos")}
+            className={activeTab === "Photos" ? "active" : ""}
+          >
             Photos
           </Tab>
-          <Tab onClick={() => handleTabClick('Videos')} className={activeTab === 'Videos' ? 'active' : ''}>
+          <Tab
+            onClick={() => handleTabClick("Videos")}
+            className={activeTab === "Videos" ? "active" : ""}
+          >
             Videos
           </Tab>
-          <Tab onClick={() => handleTabClick('Album')} className={activeTab === 'Album' ? 'active' : ''}>
+          <Tab
+            onClick={() => handleTabClick("Albums")}
+            className={activeTab === "Album" ? "active" : ""}
+          >
             Album
+          </Tab>
+          <Tab
+            onClick={() => handleTabClick("Tags")}
+            className={activeTab === "Tag" ? "active" : ""}
+          >
+            Tag
           </Tab>
           {/* Agrega más Tabs si son necesarios */}
         </Tabs>
         <ImageGrid>
-          {activeTab === 'Photos' && posts.map((post) => (
-            post.type === 'photo' && post.media && (
-              <ImageWrapper key={post.id} onClick={() => handleImageClick(post.id)}>
-                <Image src={post.media} alt={post.caption || 'Post image'} />
+          {activeTab === "Photos" &&
+            content.photos.map((photo) => (
+              <ImageWrapper
+                key={photo.id}
+                onClick={() => handleImageClick(photo.id)}
+              >
+                <Image src={photo.media} alt={photo.caption || "Photo image"} />
               </ImageWrapper>
-            )
-          ))}
-          {activeTab === 'Videos' && posts.map((post) => (
-            post.type === 'video' && post.media && (
-              <ImageWrapper key={post.id} onClick={() => handleImageClick(post.id)}>
-                <Image src={post.media} alt={post.caption || 'Post image'} />
+            ))}
+
+          {activeTab === "Videos" &&
+            content.videos.map((video) => (
+              <ImageWrapper
+                key={video.id}
+                onClick={() => handleImageClick(video.id)}
+              >
+                <video width="100%" controls>
+                  <source src={video.media} type="video/mp4" />
+                  Tu navegador no soporta la etiqueta de video.
+                </video>
               </ImageWrapper>
-            )
-          ))}
-           {activeTab === 'Album' && posts.map((post) => (
-            post.type === 'album' && post.media && (
-              <ImageWrapper key={post.id} onClick={() => handleImageClick(post.id)}>
-                <Image src={post.media} alt={post.caption || 'Post image'} />
+            ))}
+          {activeTab === "Albums" &&
+            content.albums.map((album) => (
+              <ImageWrapper
+                key={album.id}
+                onClick={() => handleImageClick(album.id)}
+              >
+                <Image src={album.media} alt={`Album ${album.id}`} />
               </ImageWrapper>
-            )
-          ))}
-          {/* Incluir más condiciones si tienes otros tipos de contenido como 'Videos', 'Album', etc. */}
+            ))}
+          {activeTab === "Tags" &&
+            content.tags.map((tag) => (
+              // Cada 'tag' es solo un post con type 'tag', así que mostramos su media directamente.
+              <ImageWrapper
+                key={tag.id}
+                onClick={() => handleImageClick(tag.id)}
+              >
+                <Image src={tag.media} alt={tag.caption || `Tag ${tag.id}`} />
+              </ImageWrapper>
+            ))}
         </ImageGrid>
       </ImageSectionContainer>
     </>
