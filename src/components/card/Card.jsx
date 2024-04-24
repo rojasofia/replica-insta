c8c493bf3215460a60ad5ad7335344c322f08316
estefania.martinez@est.iudigital.edu.co <estefania.martinez@est.iudigital.edu.co>
2024-04-24 18:09:41 -0500

    se realiza función para redireccionar las card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useAppContext } from "../../context/AppContext";
 import "./card.scss";
+import { useNavigate } from "react-router-dom";
 function Card({ post = null }) {
   const {
     user: { user, userDispatch },
   } = useAppContext();
   console.log("post", post);
+  const navigate = useNavigate();
+
+  const handleImageClick = (id) => {
+    navigate(`/details/${id}`);
+  };
   return (
     <div className="container-p">
       {post ? (
@@ -13,11 +19,19 @@ function Card({ post = null }) {
           {post.map((item, index) => (
             <article key={index} className="card-container">
               <div className="header-post">
-                <img src={item.image} alt="post" />
+                <img
+                  src={item.image}
+                  alt="post"
+                  onClick={() => handleImageClick(item.id)}
+                />
                 <p>{item.username}</p>
               </div>
               <div className="img-main">
-                <img src={item.image} alt="" />
+                <img
+                  src={item.image}
+                  alt=""
+                  onClick={() => handleImageClick(item.id)}
+                />
                 <p>
                   <b>{item.username}</b> {item.description}
                 </p>
